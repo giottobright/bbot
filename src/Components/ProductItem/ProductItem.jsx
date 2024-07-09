fcd2e0e6cb619360a50a66293d576f681c59b793
giottobright <zenbukazeroka@gmail.com>
2024-07-09 20:11:11 +0300

    1

--- src/Components/ProductItem/ProductItem.jsx
+++ src/Components/ProductItem/ProductItem.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import Button from '../Button/Button'
 import './ProductItem.css'
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea, CardActions } from '@mui/material';
 
 const ProductItem = ({product, className, onAdd}) => {
     const onAddHandler = () => {
@@ -10,17 +15,40 @@ const ProductItem = ({product, className, onAdd}) => {
 
 
   return (
-    <div className={'product'}>
-        <div className={'img'}/>
-        <div className={'title'}>{product.title}</div>
-        {/* <div className={'description'}>{product.description}</div> */}
-        <div className={'price'}>
-            <span>Стоимость: <b>{product.price}</b></span>
-        </div>
-        <Button className={'add-btn'} onClick={onAddHandler}>
-            Добавить в корзину
-        </Button>
-    </div>
+    // <div className={'product'}>
+    //     <div className={'img'}/>
+    //     <div className={'title'}>{product.title}</div>
+    //     {/* <div className={'description'}>{product.description}</div> */}
+    //     <div className={'price'}>
+    //         <span>Стоимость: <b>{product.price}</b></span>
+    //     </div>
+    //     <Button className={'add-btn'} onClick={onAddHandler}>
+    //         Добавить в корзину
+    //     </Button>
+    // </div>
+        <Card sx={{ maxWidth: 345, height: 300 }}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            height="140"
+            image=""
+            alt="green iguana"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              {product.title}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+                {product.description}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button size="small" color="primary">
+            Share
+          </Button>
+        </CardActions>
+      </Card>
     
   )
 
