58ecb3511a69af7b44c242ce76ccd3560ea75a65
giottobright <zenbukazeroka@gmail.com>
2024-07-11 16:12:25 +0300

    1

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -6,41 +6,15 @@ import TextField from '@mui/material/TextField';
 import { green } from '@mui/material/colors';
 import { Container, Grid, Card, CardContent, Typography, IconButton, InputAdornment } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
+import { Input } from '@telegram-apps/telegram-ui';
 
 const Header = (props) => {
-    const {user, onClose} = useTelegram();
+    const { user, onClose } = useTelegram();
 
     return (
-        <div className={'header'}>
+        <div className="header">
+        <TextField fullWidth id="outlined-search" label="Найти" type="search" />
             {/* <Button onClick={onClose}>Закрыть</Button> */}
-            <TextField                sx={{
-                    '& .MuiOutlinedInput-root': {
-                        '& fieldset': {
-                            borderColor: 'black',
-                        },
-                        '&:hover fieldset': {
-                            borderColor: 'black',
-                        },
-                        '&.Mui-focused fieldset': {
-                            borderColor: 'black',
-                        },
-                        height: '30px', // Set height of the input field
-                    },
-                    '& .MuiInputBase-input': {
-                        height: '30px', // Set height of the input element
-                        padding: '0 14px', // Adjust padding
-                        color: 'black',
-                    },
-                    '& .MuiInputLabel-root': {
-                        color: 'black',
-                        transition: 'transform 0.2s ease-out, font-size 0.2s ease-out',
-                        transformOrigin: 'top',
-                        '&.Mui-focused': {
-                            color: 'black',
-                            transform: 'translate(0, -0.1em) scale(0.8)', // Change the label color to white when focused
-                        },
-                    },
-                }} id="outlined-search" fullWidth label="Найти..." type="search" className='searchbar' height='30px'/>
         </div>
     );
 };
