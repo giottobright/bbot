8570832584327da7d913e2d4b605cd03f18ce316
giottobright <zenbukazeroka@gmail.com>
2024-07-19 18:22:40 +0300

    1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useCallback } from 'react';
+import React, { useEffect, useCallback, useState } from 'react';
 import { Routes, Route, useNavigate, useLocation } from 'react-router-dom';
 import { useTelegram } from './hooks/useTelegram';
 import Header from './Components/Header/Header';
@@ -14,12 +14,22 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import '@telegram-apps/telegram-ui/dist/styles.css';
-
+import Box from '@mui/material/Box';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import RestoreIcon from '@mui/icons-material/Restore';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import Paper from '@mui/material/Paper';
+import ArchiveIcon from '@mui/icons-material/Archive';
+import SportsBarIcon from '@mui/icons-material/SportsBar';
+import StorefrontIcon from '@mui/icons-material/Storefront';
 function AppContent() {
   const { tg } = useTelegram();
   const navigate = useNavigate();
   const location = useLocation();
   const { setSearchQuery } = useSearch();
+  const [value, setValue] = useState(0);
 
   const handleBackButton = useCallback(() => {
     if (location.pathname !== '/') {
@@ -47,6 +57,15 @@ function AppContent() {
     }
   }, [location.pathname, tg.BackButton]);
 
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+    if (newValue === 0) {
+      navigate('/');
+    } else if (newValue === 1) {
+      navigate('/mappage');
+    }
+  };
+
   return (
     <div className="App">
       <Header />
@@ -56,6 +75,43 @@ function AppContent() {
         <Route path="form" element={<Form />} />
         <Route path="mappage" element={<MapPage />} />
       </Routes>
+      <Box sx={{ width: '100%' }}>
+        <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0, borderRadius: '12px' }} elevation={3}>
+          <BottomNavigation
+            value={value}
+            onChange={handleChange}
+            showLabels
+            sx={{ borderRadius: '12px', backgroundColor: 'rgba(61, 26, 23, 0.8)', backdropFilter: 'blur(10px)' }}
+          >
+          <BottomNavigationAction 
+              label="Пиво" 
+              icon={<SportsBarIcon />} 
+              sx={{ 
+                color: value === 0 ? '#FFFFFF' : '#F2DDCF',
+                '&.Mui-selected': {
+                  color: '#FFFFFF'
+                },
+                '& .MuiBottomNavigationAction-label': {
+                  fontFamily: 'Comfortaa, sans-serif',
+                },
+              }} 
+            />
+            <BottomNavigationAction 
+              label="Бары" 
+              icon={<StorefrontIcon />} 
+              sx={{ 
+                color: value === 0 ? '#FFFFFF' : '#F2DDCF',
+                '&.Mui-selected': {
+                  color: '#FFFFFF'
+                },
+                '& .MuiBottomNavigationAction-label': {
+                  fontFamily: 'Comfortaa, sans-serif',
+                },
+              }} 
+            />
+          </BottomNavigation>
+        </Paper>
+      </Box>
     </div>
   );
 }
