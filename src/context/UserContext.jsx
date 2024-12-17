ae6cf74772b144c8867cd35f39b41b9af9959519
giottobright <zenbukazeroka@gmail.com>
2024-12-17 15:57:12 +0300

    6

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -8,13 +8,22 @@ export function UserProvider({ children }) {
   const { tg } = useTelegram();
 
   useEffect(() => {
-    console.log('Telegram init data:', tg.initDataUnsafe);
-    if (tg.initDataUnsafe?.user?.id) {
-      const id = tg.initDataUnsafe.user.id.toString();
-      console.log('User ID from Telegram:', id);
+    console.log('Telegram object:', tg);
+    console.log('User from useTelegram:', user);
+    console.log('InitData:', window.Telegram.WebApp.initData);
+    console.log('InitDataUnsafe:', window.Telegram.WebApp.initDataUnsafe);
+    
+    // Пробуем получить ID разными способами
+    const telegramUser = window.Telegram.WebApp.initDataUnsafe?.user || user || tg.initDataUnsafe?.user;
+    
+    if (telegramUser?.id) {
+      const id = telegramUser.id.toString();
+      console.log('Found user ID:', id);
       setUserId(id);
+    } else {
+      console.log('User ID not found in any source');
     }
-  }, [tg.initDataUnsafe]);
+  }, [tg.initDataUnsafe, user]);
 
   return (
     <UserContext.Provider value={{ userId }}>
